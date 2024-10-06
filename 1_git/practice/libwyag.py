1a6105f9aca3200026f24bf04b795f26facc6170
root <root@lewiea>
2024-10-06 20:05:57 +0800

    stash

--- 1_git/practice/libwyag.py
+++ 1_git/practice/libwyag.py
@@ -28,9 +28,8 @@ import os
 import re
 import sys
 import zlib
-from libgitrepo import GitRepository, find_git_repo
+from libgitrepo import GitRepository, GitIgnore, GitIndex, GitIndexEntry, find_git_repo
 from libgitobj import GitBlob, GitCommit, GitTree, GitTreeLeaf, GitTag
-
 argparser = argparse.ArgumentParser(description="The stupidest content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
@@ -65,14 +64,12 @@ argsp.add_argument("path",
                    default=".",
                    help="Where to create the repository.")
 
-def cmd_init(args):
+def cmd_init(args) -> GitRepository:
     """Create a new repository at path."""
     path = os.path.abspath('./')
-    repo = GitRepository(path, True)
-    repo.repo_init(path)
+    repo:GitRepository = GitRepository(path, True)
+    repo.init_repo(path)
     return repo
-
-
 
 def object_read(repo, sha):
     """Read object sha from Git repository repo.  Return a
@@ -546,193 +543,13 @@ def cmd_rev_parse(args):
 
     print (object_find(repo, args.name, fmt, follow=True))
 
-class GitIndexEntry (object):
-    def __init__(self, ctime=None, mtime=None, dev=None, ino=None,
-                 mode_type=None, mode_perms=None, uid=None, gid=None,
-                 fsize=None, sha=None, flag_assume_valid=None,
-                 flag_stage=None, name=None):
-      # The last time a file's metadata changed.  This is a pair
-      # (timestamp in seconds, nanoseconds)
-      self.ctime = ctime
-      # The last time a file's data changed.  This is a pair
-      # (timestamp in seconds, nanoseconds)
-      self.mtime = mtime
-      # The ID of device containing this file
-      self.dev = dev
-      # The file's inode number
-      self.ino = ino
-      # The object type, either b1000 (regular), b1010 (symlink),
-      # b1110 (gitlink).
-      self.mode_type = mode_type
-      # The object permissions, an integer.
-      self.mode_perms = mode_perms
-      # User ID of owner
-      self.uid = uid
-      # Group ID of ownner
-      self.gid = gid
-      # Size of this object, in bytes
-      self.fsize = fsize
-      # The object's SHA
-      self.sha = sha
-      self.flag_assume_valid = flag_assume_valid
-      self.flag_stage = flag_stage
-      # Name of the object (full path this time!)
-      self.name = name
-
-class GitIndex (object):
-    version = None
-    entries = []
-    # ext = None
-    # sha = None
-
-    def __init__(self, version=2, entries=None):
-        if not entries:
-            entries = list()
-
-        self.version = version
-        self.entries = entries
-
-def index_read(repo):
-    """
-    Read the index file of a Git repository and return a GitIndex object.
-
-    Args:
-        repo (str): The path to the Git repository.
-
-    Returns:
-        GitIndex: The GitIndex object representing the index file.
-
-    Raises:
-        AssertionError: If the index file version is not supported.
-
-    """
-    index_file = repo.create_filerepo("index")
-
-    # New repositories have no index!
-    if not os.path.exists(index_file):
-        return GitIndex()
-
-    with open(index_file, 'rb') as f:
-        raw = f.read()
-
-    # Rest of the code...
-
-    return GitIndex(version="version", entries="entries")
-def index_read(repo):
-    index_file = repo.create_filerepo("index")
-
-    # New repositories have no index!
-    if not os.path.exists(index_file):
-        return GitIndex()
-
-    with open(index_file, 'rb') as f:
-        raw = f.read()
-
-    header = raw[:12]
-    signature = header[:4]
-    assert signature == b"DIRC" # Stands for "DirCache"
-    version = int.from_bytes(header[4:8], "big")
-    assert version == 2, "wyag only supports index file version 2"
-    count = int.from_bytes(header[8:12], "big")
-
-    entries = list()
-
-    content = raw[12:]
-    idx = 0
-    for i in range(0, count):
-        # Read creation time, as a unix timestamp (seconds since
-        # 1970-01-01 00:00:00, the "epoch")
-        ctime_s =  int.from_bytes(content[idx: idx+4], "big")
-        # Read creation time, as nanoseconds after that timestamps,
-        # for extra precision.
-        ctime_ns = int.from_bytes(content[idx+4: idx+8], "big")
-        # Same for modification time: first seconds from epoch.
-        mtime_s = int.from_bytes(content[idx+8: idx+12], "big")
-        # Then extra nanoseconds
-        mtime_ns = int.from_bytes(content[idx+12: idx+16], "big")
-        # Device ID
-        dev = int.from_bytes(content[idx+16: idx+20], "big")
-        # Inode
-        ino = int.from_bytes(content[idx+20: idx+24], "big")
-        # Ignored.
-        unused = int.from_bytes(content[idx+24: idx+26], "big")
-        assert 0 == unused
-        mode = int.from_bytes(content[idx+26: idx+28], "big")
-        mode_type = mode >> 12
-        assert mode_type in [0b1000, 0b1010, 0b1110]
-        mode_perms = mode & 0b0000000111111111
-        # User ID
-        uid = int.from_bytes(content[idx+28: idx+32], "big")
-        # Group ID
-        gid = int.from_bytes(content[idx+32: idx+36], "big")
-        # Size
-        fsize = int.from_bytes(content[idx+36: idx+40], "big")
-        # SHA (object ID).  We'll store it as a lowercase hex string
-        # for consistency.
-        sha = format(int.from_bytes(content[idx+40: idx+60], "big"), "040x")
-        # Flags we're going to ignore
-        flags = int.from_bytes(content[idx+60: idx+62], "big")
-        # Parse flags
-        flag_assume_valid = (flags & 0b1000000000000000) != 0
-        flag_extended = (flags & 0b0100000000000000) != 0
-        assert not flag_extended
-        flag_stage =  flags & 0b0011000000000000
-        # Length of the name.  This is stored on 12 bits, some max
-        # value is 0xFFF, 4095.  Since names can occasionally go
-        # beyond that length, git treats 0xFFF as meaning at least
-        # 0xFFF, and looks for the final 0x00 to find the end of the
-        # name --- at a small, and probably very rare, performance
-        # cost.
-        name_length = flags & 0b0000111111111111
-
-        # We've read 62 bytes so far.
-        idx += 62
-
-        if name_length < 0xFFF:
-            assert content[idx + name_length] == 0x00
-            raw_name = content[idx:idx+name_length]
-            idx += name_length + 1
-        else:
-            print("Notice: Name is 0x{:X} bytes long.".format(name_length))
-            # This probably wasn't tested enough.  It works with a
-            # path of exactly 0xFFF bytes.  Any extra bytes broke
-            # something between git, my shell and my filesystem.
-            null_idx = content.find(b'\x00', idx + 0xFFF)
-            raw_name = content[idx: null_idx]
-            idx = null_idx + 1
-
-        # Just parse the name as utf8.
-        name = raw_name.decode("utf8")
-
-        # Data is padded on multiples of eight bytes for pointer
-        # alignment, so we skip as many bytes as we need for the next
-        # read to start at the right position.
-
-        idx = 8 * ceil(idx / 8)
-
-        # And we add this entry to our list.
-        entries.append(GitIndexEntry(ctime=(ctime_s, ctime_ns),
-                                     mtime=(mtime_s,  mtime_ns),
-                                     dev=dev,
-                                     ino=ino,
-                                     mode_type=mode_type,
-                                     mode_perms=mode_perms,
-                                     uid=uid,
-                                     gid=gid,
-                                     fsize=fsize,
-                                     sha=sha,
-                                     flag_assume_valid=flag_assume_valid,
-                                     flag_stage=flag_stage,
-                                     name=name))
-
-    return GitIndex(version=version, entries=entries)
 
 argsp = argsubparsers.add_parser("ls-files", help = "List all the stage files")
 argsp.add_argument("--verbose", action="store_true", help="Show everything.")
 
 def cmd_ls_files(args):
   repo = find_git_repo()
-  index = index_read(repo)
+  index = repo.read_index()
   if args.verbose:
     print("Index file format v{}, containing {} entries.".format(index.version, len(index.entries)))
 
@@ -792,13 +609,7 @@ def gitignore_parse(lines):
 
     return ret
 
-class GitIgnore(object):
-    absolute = None
-    scoped = None
 
-    def __init__(self, absolute, scoped):
-        self.absolute = absolute
-        self.scoped = scoped
 
 def gitignore_read(repo):
     ret = GitIgnore(absolute=list(), scoped=dict())
@@ -821,7 +632,7 @@ def gitignore_read(repo):
             ret.absolute.append(gitignore_parse(f.readlines()))
 
     # .gitignore files in the index
-    index = index_read(repo)
+    index = repo.read_index()
 
     for entry in index.entries:
         if entry.name == ".gitignore" or entry.name.endswith("/.gitignore"):
@@ -872,7 +683,7 @@ argsp = argsubparsers.add_parser("status", help = "Show the working tree status.
 
 def cmd_status(_):
     repo = find_git_repo()
-    index = index_read(repo)
+    index = repo.read_index()
 
     cmd_status_branch(repo)
     cmd_status_head_index(repo, index)
@@ -992,7 +803,7 @@ def cmd_status_index_worktree(repo, index):
         if not check_ignore(ignore, f):
             print(" ", f)
 
-def index_write(repo, index):
+def write_index(repo, index):
     with open(repo.create_filerepo("index"), "wb") as f:
 
         # HEADER
@@ -1055,57 +866,21 @@ argsp = argsubparsers.add_parser("rm", help="Remove files from the working tree 
 argsp.add_argument("path", nargs="+", help="Files to remove")
 
 def cmd_rm(args):
-  repo = find_git_repo()
-  rm(repo, args.path)
-
-def rm(repo, paths, delete=True, skip_missing=False):
-  # Find and read the index
-  index = index_read(repo)
-
-  worktree = repo.worktree + os.sep
-
-  # Make paths absolute
-  abspaths = list()
-  for path in paths:
-    abspath = os.path.abspath(path)
-    if abspath.startswith(worktree):
-      abspaths.append(abspath)
-    else:
-      raise Exception("Cannot remove paths outside of worktree: {}".format(paths))
-
-  kept_entries = list()
-  remove = list()
-
-  for e in index.entries:
-    full_path = os.path.join(repo.worktree, e.name)
-
-    if full_path in abspaths:
-      remove.append(full_path)
-      abspaths.remove(full_path)
-    else:
-      kept_entries.append(e) # Preserve entry
-
-  if len(abspaths) > 0 and not skip_missing:
-    raise Exception("Cannot remove paths not in the index: {}".format(abspaths))
-
-  if delete:
-    for path in remove:
-      os.unlink(path)
-
-  index.entries = kept_entries
-  index_write(repo, index)
+  repo:GitRepository = find_git_repo()
+  repo.rm_path(repo, args.path)
 
 argsp = argsubparsers.add_parser("add", help = "Add files contents to the index.")
 argsp.add_argument("path", nargs="+", help="Files to add")
 
-def cmd_add(args):
-  repo = find_git_repo()
+def cmd_add(args) -> GitRepository:
+  repo:GitRepository = find_git_repo()
   add(repo, args.path)
+  return repo
 
 def add(repo, paths, delete=True, skip_missing=False):
 
   # First remove all paths from the index, if they exist.
-  rm (repo, paths, delete=False, skip_missing=True)
+  repo.rm_path(repo, paths, delete=False, skip_missing=True)
 
   worktree = repo.worktree + os.sep
 
@@ -1125,7 +900,7 @@ def add(repo, paths, delete=True, skip_missing=False):
     # @FIXME, though: we could just
     # move the index through commands instead of reading and writing
     # it over again.
-    index = index_read(repo)
+    index = repo.read_index()
 
     for (abspath, relpath) in clean_paths:
       with open(abspath, "rb") as fd:
@@ -1145,7 +920,7 @@ def add(repo, paths, delete=True, skip_missing=False):
       index.entries.append(entry)
 
     # Write the index back
-    index_write(repo, index)
+    write_index(repo, index)
 
 argsp = argsubparsers.add_parser("commit", help="Record changes to the repository.")
 
@@ -1256,7 +1031,7 @@ def commit_create(repo, tree, parent, author, timestamp, message):
 
 def cmd_commit(args):
     repo = find_git_repo()
-    index = index_read(repo)
+    index = repo.read_index()
     # Create trees, grab back SHA for the root tree.
     tree = tree_from_index(repo, index)
 
